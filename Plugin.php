6777ba50675f57c2831858626612266c8a5bca2c
Lucas Zamora <lucaszdv@gmail.com>
2019-02-16 18:17:14 -0300

    New version deprecated tables and logic.

--- Plugin.php
+++ Plugin.php
@@ -1,43 +1,16 @@
-<?php namespace Awebsome\RemoteSsh;
+<?php namespace Awebsome\Ssh;
 
-
-use App;
-use Config;
 use System\Classes\PluginBase;
-use Illuminate\Foundation\AliasLoader;
-
-use Awebsome\RemoteSsh\Models\Settings as CFG;
 
 class Plugin extends PluginBase
 {
     public function boot()
     {
         // Register ServiceProviders
-        App::register('Collective\Remote\RemoteServiceProvider');
+        \App::register('Collective\Remote\RemoteServiceProvider');
 
         // Register aliases
-        $alias = AliasLoader::getInstance();
-        $alias->alias('SSH2', 'Collective\Remote\RemoteFacade');
-    }
-
-    public function registerComponents()
-    {
-    }
-
-    public function registerSettings()
-    {
-        return [
-            //Connection Settings
-            /*'remotessh'  => [
-                'label'       => 'Remote SSH',
-                'description' => 'Settings of Remote SSH',
-                'category'    => 'Remote SSH',
-                'icon'        => 'icon-terminal',
-                'class'       => 'Awebsome\RemoteSsh\Models\Settings',
-                'order'       => 100,
-                'permissions' => [ 'awebsome.remotessh.settings' ],
-                'keywords'    => 'Server Remote Ssh'
-            ],*/
-        ];
+        $alias = \Illuminate\Foundation\AliasLoader::getInstance();
+        $alias->alias('SSH', 'Collective\Remote\RemoteFacade');
     }
 }
